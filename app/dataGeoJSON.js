c3d4268aa3dd1fde2898b214f8fdd2cc027f4be8
Rodrigo <28023409+roicort@users.noreply.github.com>
2020-04-30 14:18:58 -0500

    Fuzzy Search

--- app/dataGeoJSON.js
+++ app/dataGeoJSON.js
@@ -5,68 +5,22 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "Aves Plaza Principal",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "location":"Reynosa",
+          "date": "26-01-2020",
+          "time": "12:00",
+          "device": "Zoom H6",
+          "duration": "6:12",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gerardo Meza",
           "url":"https://soundcloud.com/topografias_sonicas/aves-plaza-principal"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Grillos En La Ciudad",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
-          "extension": "wav",
-          "samplerate": "44100hz",
-          "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/grillos-en-la-ciudad"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            10.0,
-            10.0
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Ruidos De La Plaza",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
-          "extension": "wav",
-          "samplerate": "44100hz",
-          "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/ruidos-de-la-plaza"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            -10.0,
-            -10.0
+            26.09276,
+            -98.27759
           ]
         }
       },
@@ -74,68 +28,46 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "Avión pasando por la colonia Unidad Nacional",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "location":"Ciudad Madero",
+          "date": "09-11-2019",
+          "time": "18:20",
+          "device": "Huawei mate 20 lite",
+          "duration": "1:23",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gloria Gómez",
           "url":"https://soundcloud.com/topografias_sonicas/avion-pasando-por-unidad-nacional"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            100.0,
-            0.0
+            22.27078,
+            -97.85067
           ]
         }
       },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Pieza Barco Saliendo Del Rio Pánico",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
-          "extension": "wav",
-          "samplerate": "44100hz",
-          "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/pieza-barco-saliendo-del-rio-panico"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            80.0,
-            0.0
-          ]
-        }
-      },
+
       {
         "type": "Feature",
         "properties": {
           "name": "Escolleras Miramar",
-          "location":"Unknown",
+          "location":"Playa Miramar",
           "date": "DD-MM-YYYY",
           "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "device": "Celular",
+          "duration": "1:02",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gerardo Meza",
           "url":"https://soundcloud.com/topografias_sonicas/escolleras-miramar"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -80.0,
-            0.0
+            22.26426,
+            -97.78434
           ]
         }
       },
@@ -143,22 +75,22 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "Los Diamantes",
-          "location":"Unknown",
+          "location":"San Fernando",
           "date": "DD-MM-YYYY",
           "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "device": "celular",
+          "duration": "2:58",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Emmanuel Zepeda",
           "url":"https://soundcloud.com/topografias_sonicas/los-diamantes"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -90.0,
-            0.0
+            24.86011,
+            -98.16457
           ]
         }
       },
@@ -166,22 +98,22 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "El Huasteco Veracruzano",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "location":"Plaza de Armas",
+          "date": "09-11-2019",
+          "time": "19:00",
+          "device": "Huawei mate 20 lite",
+          "duration": "2:30",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gloria Gómez",
           "url":"https://soundcloud.com/topografias_sonicas/el-huasteco-veracruzano"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -100.0,
-            0.0
+            22.21536,
+            -97.85801
           ]
         }
       },
@@ -189,22 +121,22 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "LuchaLibre",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "location":"Lucha Libre EMLL",
+          "date": "03-11-2019",
+          "time": "21:10",
+          "device": "Iphone 7",
+          "duration": "0:56",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/luchalibre-1"
+          "author": "Tania Castro",
+          "url":"https://soundcloud.com/topografias_sonicas/luchalibre/"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -100.0,
-            22.0
+            22.21628,
+            -97.85325
           ]
         }
       },
@@ -212,22 +144,22 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "PresentaciónLuchaLibre",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "location":"Lucha Libre EMLL",
+          "date": "03-11-2019",
+          "time": "17:30",
+          "device": "Iphone /",
+          "duration": "0:16",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Tania Castro",
           "url":"https://soundcloud.com/topografias_sonicas/presentacio-nluchalibre-1"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -100.0,
-            20.0
+            22.21628,
+            -97.85325
           ]
         }
       },
@@ -235,91 +167,68 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "Mercado Zaragoza",
-          "location":"Unknown",
+          "location":"Reynosa",
           "date": "DD-MM-YYYY",
           "time": "00:00",
           "device": "device",
-          "duration": "1:00",
+          "duration": "2:10",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Josue Alonzo",
           "url":"https://soundcloud.com/topografias_sonicas/mercado-zaragoza-2"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
+            26.09103,
+            -98.27962
           ]
         }
       },
       {
         "type": "Feature",
         "properties": {
-          "name": "Plaza de Armas Tampico",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "name": "Danzón en la plaza de armas de Tampico",
+          "location":"Plaza de Armas",
+          "date": "15-08-2016",
+          "hour": "17:30",
+          "equipment": "celular",
+          "duration": "0:59",
           "extension": "wav",
-          "samplerate": "44100hz",
+          "samplerate": "44100",
           "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/plaza-de-armas-tampico-1"
+          "author": "Mario Rodriguez",
+          "url":"https://soundcloud.com/topografias_sonicas/plaza-de-armas-tampico"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
+            22.21575782,
+            -97.857768
           ]
         }
       },
       {
         "type": "Feature",
         "properties": {
-          "name": "Pajaros",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "name": "Pajaros del Boulevard",
+          "location":"Boulevard Adolfo Lopez",
+          "date": "10-11-2019",
+          "time": "06:30",
+          "device": "Huawei mate 20 lite",
+          "duration": "5:51",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/pajaros"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.0,
-            0.0
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Nidal De Pájaros Boulevard Adolfo Lopez",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
-          "extension": "wav",
-          "samplerate": "44100hz",
-          "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gloria Gómez",
           "url":"https://soundcloud.com/topografias_sonicas/nidal-de-pajaros-boulevard-adolfo-lopez"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
+            22.27301,
+            -97.85115
           ]
         }
       },
@@ -327,45 +236,22 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "Marimba Andador Salvador Díaz Mirón",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "location":"Sears",
+          "date": "09-11-2019",
+          "time": "18:15",
+          "device": "Huawei mate 20 lite",
+          "duration": "1:09",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gloria Gómez",
           "url":"https://soundcloud.com/topografias_sonicas/marimba-andador-salvador-diaz-miron"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Mercado Zaragoza",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
-          "extension": "wav",
-          "samplerate": "44100hz",
-          "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/mercado-zaragoza-1"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.0,
-            0.0
+            22.21494,
+            -97.85719
           ]
         }
       },
@@ -377,41 +263,18 @@ var dataGeoJSON = {
           "date": "DD-MM-YYYY",
           "time": "00:00",
           "device": "device",
-          "duration": "1:00",
+          "duration": "4:06",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Juan Perez",
           "url":"https://soundcloud.com/topografias_sonicas/mercado-zaragoza"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
-          ]
-        }
-      },
-      {
-        "type": "Feature",
-        "properties": {
-          "name": "Plaza de armas Tampico",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
-          "extension": "wav",
-          "samplerate": "44100hz",
-          "bitdepth": "24",
-          "author": "John Doe",
-          "url":"https://soundcloud.com/topografias_sonicas/plaza-de-armas-tampico"
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            0.0,
-            0.0
+            26.09114,
+            -98.27961
           ]
         }
       },
@@ -419,45 +282,45 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "Barco saliendo del río Pánuco",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
+          "location":"Escolleras",
+          "date": "27-09-2016",
           "time": "00:00",
           "device": "device",
-          "duration": "1:00",
+          "duration": "2:00",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Juan Perez",
           "url":"https://soundcloud.com/topografias_sonicas/farodelasescolleras"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
+            22.26557,
+            -97.77493
           ]
         }
       },
       {
         "type": "Feature",
         "properties": {
-          "name": "Aduana de Noche",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "name": "La Aduana de Noche",
+          "location":"Tortas de la barda",
+          "date": "09-11-2019",
+          "time": "19:00",
+          "device": "Huawei mate 20 lite",
+          "duration": "1:03",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gloria Gómez",
           "url":"https://soundcloud.com/topografias_sonicas/aduana-de-noche"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
+            22.2121,
+            -97.85762
           ]
         }
       },
@@ -465,15 +328,15 @@ var dataGeoJSON = {
         "type": "Feature",
         "properties": {
           "name": "Calle Benito Juárez",
-          "location":"Unknown",
-          "date": "DD-MM-YYYY",
-          "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "location":"Calle Benito Juárez",
+          "date": "09-11-2019",
+          "time": "19:00",
+          "device": "Huawei mate 20 lite",
+          "duration": "3:04",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Gerardo Meza",
           "url":"https://soundcloud.com/topografias_sonicas/calle-benito"
         },
         "geometry": {
@@ -489,21 +352,67 @@ var dataGeoJSON = {
         "properties": {
           "name": "Maniobra Puerto Altamira",
           "location":"Unknown",
-          "date": "DD-MM-YYYY",
+          "date": "09-02-2018",
           "time": "00:00",
-          "device": "device",
-          "duration": "1:00",
+          "device": "celular",
+          "duration": "2:39",
           "extension": "wav",
           "samplerate": "44100hz",
           "bitdepth": "24",
-          "author": "John Doe",
+          "author": "Juan Perez",
           "url":"https://soundcloud.com/topografias_sonicas/maniobra-puerto-altamira"
         },
         "geometry": {
           "type": "Point",
           "coordinates": [
-            0.0,
-            0.0
+            22.37648,
+            -97.89199
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Aves Plaza Principal",
+          "location":"Reynosa",
+          "date": "26-01-2020",
+          "time": "12:00",
+          "device": "Zoom H6",
+          "duration": "6:12",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/aves-plaza-principal"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.09276,
+            -98.27759
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Preludio a la Banda Municipal",
+          "location":"Reynosa",
+          "date": "26-01-2020",
+          "time": "14:00",
+          "device": "Zoom H6",
+          "duration": "6:43",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/preludio-a-la-banda-municipal"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.09302,
+            -98.278
           ]
         }
       }
