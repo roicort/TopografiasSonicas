29e63f81118d7a2599aa6d339f1a8820a37f2b22
Rodrigo <28023409+roicort@users.noreply.github.com>
2020-05-16 12:12:04 -0500

    New Data

--- app/dataGeoJSON.js
+++ app/dataGeoJSON.js
@@ -342,8 +342,8 @@ var dataGeoJSON = {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            19.432608,
-            -99.133209
+            22.21539,
+            -97.85645
           ]
         }
       },
@@ -413,6 +413,443 @@ var dataGeoJSON = {
           "coordinates": [
             26.09302,
             -98.278
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Antigua estación del ferrocarril",
+          "location":"Reynosa",
+          "date": "26-01-2020",
+          "time": "13:30",
+          "device": "Zoom H6",
+          "duration": "7:25",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/antigua-estacion-del-ferrocarril"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.08783,
+            -98.28039
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Antigua estación del ferrocarril",
+          "location":"Reynosa",
+          "date": "26-01-2020",
+          "time": "13:30",
+          "device": "Zoom H6",
+          "duration": "7:25",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/antigua-estacion-del-ferrocarril"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.08783,
+            -98.28039
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Estética Italia",
+          "location":"C.Hidalgo",
+          "date": "26-01-2020",
+          "time": "13:46",
+          "device": "Zoom H6",
+          "duration": "3:36",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/esteticas-italia"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.09096,
+            -98.27884
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Transformador, aves y gente",
+          "location":"Parque Cultural Reynosa",
+          "date": "25-01-2020",
+          "time": "13:35",
+          "device": "Zoom H6",
+          "duration": "2:04",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/esteticas-italia"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.05093,
+            -98.27026
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Maquinas de comida",
+          "location":"Parque Cultural Reynosa",
+          "date": "25-01-2020",
+          "time": "13:35",
+          "device": "Zoom H6",
+          "duration": "0:42",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Adalberto Cantero Torres",
+          "url":"https://soundcloud.com/topografias_sonicas/maquinas-de-comida"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.05028,
+            -98.2697
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sesión de fotos",
+          "location":"Parque Cultural Reynosa",
+          "date": "25-01-2020",
+          "time": "-",
+          "device": "Zoom H6",
+          "duration": "0:50",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Adalberto Cantero Torres",
+          "url":"https://soundcloud.com/topografias_sonicas/sesion-de-fotos"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.05073,
+            -98.26919
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Kiosko plaza Miguel Hidalgo",
+          "location":"Plaza Miguel Hidalgo",
+          "date": "26-01-2020",
+          "time": "12:00",
+          "device": "Zoom H6",
+          "duration": "3:49",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Adalberto Cantero Torres",
+          "url":"https://soundcloud.com/topografias_sonicas/kiosko-plaza-miguel-hidalgo"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.09284,
+            -98.27802
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Fuentes plaza Miguel Hidalgo",
+          "location":"Plaza Miguel Hidalgo",
+          "date": "26-01-2020",
+          "time": "12:40",
+          "device": "Zoom H6",
+          "duration": "2:11",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Adalberto Cantero Torres",
+          "url":"https://soundcloud.com/topografias_sonicas/fuentes-plaza-miguel-hidalgo"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.09248,
+            -98.27769
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Palacio Municipal y payasos",
+          "location":"Plaza Miguel Hidalgo",
+          "date": "26-01-2020",
+          "time": "12:44",
+          "device": "Zoom H6",
+          "duration": "2:48",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author":"Gerardo Meza" ,
+          "url":"https://soundcloud.com/topografias_sonicas/palacio-municipal-y-payasos"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.09245,
+            -98.27816
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Entrada al Parque Cultural",
+          "location":"Parque Cultural Reynosa",
+          "date": "25-01-2020",
+          "time": "12:44",
+          "device": "Zoom H6",
+          "duration": "9:53",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/entrada-parque-cultural"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            26.04981,
+            -98.226947
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mercado de la puntilla",
+          "location":"Centro de tampico",
+          "date": "18-01-2020",
+          "time": "12:44",
+          "device": "Zoom H6",
+          "duration": "3:15",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/mercado-de-la-puntilla"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.2153,
+            -97.86496
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Plaza de la Libertad",
+          "location":"Centro de tampico",
+          "date": "07-01-2020",
+          "time": "12:44",
+          "device": "Zoom H6",
+          "duration": "1:33",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/plaza-de-la-libertad"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.21348,
+            -97.8569
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Tortas de la Barda",
+          "location":"Plaza Hijas de Tampico",
+          "date": "07-01-2020",
+          "time": "15:32",
+          "device": "Zoom H6",
+          "duration": "3:29",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/tortas-de-la-barda"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.21198,
+            -97.85744
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Por el mercado",
+          "location":"Mercado de Tampico",
+          "date": "07-01-2020",
+          "time": "15:38",
+          "device": "Zoom H6",
+          "duration": "6:58",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/por-el-mercado"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.21291,
+            -97.85879
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Una tarde en la plaza",
+          "location":"Plaza de Armas",
+          "date": "07-01-2020",
+          "time": "16:05",
+          "device": "Zoom H6",
+          "duration": "2:29",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Gerardo Meza",
+          "url":"https://soundcloud.com/topografias_sonicas/una-tarde-en-la-plaza"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.21562,
+            -97.85782
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Frente a la plaza",
+          "location":"Plaza Isauro Alfaro",
+          "date": "20-01-2020",
+          "time": "-",
+          "device": "Huawei",
+          "duration": "0:38",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Frida García",
+          "url":"https://soundcloud.com/topografias_sonicas/frente-a-la-plaza"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.2471,
+            -97.83673
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Checador de Precios",
+          "location":"Arteli",
+          "date": "20-01-2020",
+          "time": "-",
+          "device": "Celular",
+          "duration": "0:19",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Lucero Arreola",
+          "url":"https://soundcloud.com/topografias_sonicas/checador-de-precios"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.24443,
+            -97.8369
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Coladera",
+          "location":"Centro de Altamira",
+          "date": "20-01-2020",
+          "time": "-",
+          "device": "Celular",
+          "duration": "0:55",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Chuy Carrillo",
+          "url":"https://soundcloud.com/topografias_sonicas/coladera"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.40789,
+            -97.92117
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bar la Roca",
+          "location":"Ciudad Madero",
+          "date": "20-01-2020",
+          "time": "19:33",
+          "device": "Celular",
+          "duration": "0:41",
+          "extension": "wav",
+          "samplerate": "48000hz",
+          "bitdepth": "24",
+          "author": "Daniela Campos",
+          "url":"https://soundcloud.com/topografias_sonicas/bar-la-roca"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            22.2686,
+            -97.84573
           ]
         }
       }
